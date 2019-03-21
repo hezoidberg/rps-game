4f31a90311cae102ff0f78c33805c13380e93cc2
hezoidberg <batilovnf@gmail.com>
2019-03-21 22:52:12 +0300

    Update css.

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,25 +1,27 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
 //= require jquery3
 //= require popper
 //= require bootstrap
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
-})
+});
 
 function winner(comparator) {
   var msg = '';
   switch(comparator) {
     case 1: 
-      msg = 'you win!';
+      msg = 'YOU WIN!'; 
       break;
     case -1:
-      msg = 'you lost!';
+      msg = 'YOU LOST!';
       break;
     default:
-      msg = 'draw';
+      msg = 'DRAW!';
   }
   return msg;
+}
+
+function image_path(type) {
+  return '/assets/' + type.charAt(0).toUpperCase() + type.slice(1);
 }
 
 $(document).ready(function() {
@@ -28,15 +30,18 @@ $(document).ready(function() {
       $.ajax({
           url: "/games/throw/" + $(this).attr('id'),
           beforeSend: function() {
+            $('#comp-result').addClass('d-none');
             $('#user-pick').text(type);
-            $('.spinner-border').removeClass('d-none');
+            $('.spinner').removeClass('d-none');
+            $('#result-user-img').attr('src', image_path(type));
           },
           success: function(msg) {
-            $('#comp-pick').text(msg['comp_pick']['type']);
-            $('#result').text(winner(msg['winner']))
+            $('#result').text(winner(msg['winner']));
+            $('#result-comp-img').attr('src', image_path(msg['comp_pick']['type']));
+            $('#comp-result').removeClass('d-none');
           },
           complete: function() {
-            $('.spinner-border').addClass('d-none');
+            $('.spinner').addClass('d-none');
           }
       })
   });
